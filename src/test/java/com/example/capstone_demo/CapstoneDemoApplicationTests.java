e5849a84fb34de3cfc01f596f640726250277281
Marikumar <marikumarnagaraj2002@gmail.com>
2026-02-15 18:39:22 +0530

    first commit

---
+++ src/test/java/com/example/capstone_demo/CapstoneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
